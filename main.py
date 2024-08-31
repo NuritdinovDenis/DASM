b96eb2b4c05685a9fde364348dd2c9360d674dc5
NuritdinovDenis <53662240+NuritdinovDenis@users.noreply.github.com>
2024-08-31 21:27:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def assemble(file_input, file_output):
+    instruction_set = {
+        "mov ax, bx": b'\x89\xD8',  код)
+        "add ax, bx": b'\x01\xD8'  
+    }
+    
+    with open(file_input, 'r') as infile, open(file_output, 'wb') as outfile:
+        for line in infile:
+            line = line.strip()
+            print(line)
+            if line in instruction_set:
+                outfile.write(instruction_set[line])
+            else:
+                print(f"Неизвестная инструкция: {line}")
+
+assemble('input.asm', 'output.bin')
